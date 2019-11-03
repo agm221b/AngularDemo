45baaba3c372139f866eb9ae642e88602aa67300
MayankRaj2 <mayank2497@gmail.com>
2019-11-03 21:02:23 +0530

    sorting done

--- BRSSpringBootAng/src/app/app.viewalluserscomponent.ts
+++ BRSSpringBootAng/src/app/app.viewalluserscomponent.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit } from '@angular/core'
 import { User } from './_model/app.user';
 import { UserService } from './_service/app.userservice';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'viewallusers',
@@ -10,13 +11,29 @@ export class ViewAllUsersComponent implements OnInit{
     user: User;
     id;
     users: User[]=[];
-    constructor(private userservice:UserService){
+    constructor(private userservice:UserService, private toastr: ToastrService){
         console.log("In constructor");
     }
     sub;
     ngOnInit(): void {
         console.log("NG On Init");
         this.userservice.viewAllUsers().subscribe((data:User[])=>this.users=data);
+    }
+
+    removeUser(id: any): any {
+        console.log(id);
+        this.userservice.removeUser(id).subscribe();
+        this.toastr.success('User Deleted Successfully!', 'Congratulations!');
+        location.reload();
+        
+        
+    }
+
+    sortById(){
+        this.users.sort(
+            (val1, val2)=>
+            val1.userId-val2.userId
+        );
     }
 
 }
